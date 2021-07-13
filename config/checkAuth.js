ed31647436a8f588080374c2216c693f42a2d3c7
slavastafiyuk <73066939+slavastafiyuk@users.noreply.github.com>
2021-07-13 16:39:45 +0100

    update

--- config/checkAuth.js
+++ config/checkAuth.js
@@ -3,8 +3,6 @@
 function ensureAuthenticated(req, res, next) {
     if (!req.isAuthenticated()) {
         return res.redirect('/');
-    }else if(req.isAuthenticated()){
-
     }
      return next();
 }
